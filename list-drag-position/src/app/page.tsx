8baa0f04ec10f3492cc327ba31e46a00d186817f
_sameer <sameer.mishra@infra.market>
2024-07-12 12:21:17 +0530

    added the component for the adding multiple cards and able to randomly place them with making them draggable with changing their position after being dragged

--- list-drag-position/src/app/page.tsx
+++ list-drag-position/src/app/page.tsx
@@ -1,19 +1,74 @@
 "use client"
+import {DndProvider} from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import { useState } from "react"
+import { HEIGHT, WIDTH } from './constants';
+
+class ItemType {
+    id=0;
+    message='';
+    x=0;
+    y=0;
+
+    constructor(id:number,message:string){
+        this.id=id;
+        this.message=message;
+        this.x=Math.floor(Math.random()*(WIDTH-384));
+        this.y=Math.floor(Math.random()*(HEIGHT-128));
+    }
+    updatePosition(x:number, y:number){
+        this.x = x;
+        this.y = y;
+    }
+}
 
 export default function Home() {
-    const [item, setItem] = useState('');
+    const [item, setItem] = useState<string>('');
+    const [list, setList] = useState<ItemType[]>([]);
     const handleChange = (e) => {
         setItem(e.target.value);
     }
     const handleAddItem = () => {
-
+        if(item.length > 0){
+            setList(prev => [
+                ...prev,
+                new ItemType(prev.length, item)
+            ]);
+            setItem('');
+        }
+    }
+    const handleDragOver = (e) => {
+        console.log('dragOver', e);
+    }
+    const handleDrag = (e) => {
+        console.log('handleDrag', e);
+    }
+    const handleDragEnd = (e) => {
+        console.log('handleDragEnd', e);
+        // const newList = list.map((item) => {
+        //     if(item.id == parseInt(e.target.id)){
+        //         item.updatePosition(e.screenX,e.screenY);
+        //     } 
+        //     return item;
+        // });
+        console.log(e.screenX,':X    Y:',e.screenY)
+        setList(prev => prev.map((item) => {
+            if(item.id == parseInt(e.target.id)){
+                item.updatePosition(e.screenX,e.screenY);
+            } 
+            return item;
+        }));
     }
     
-    return <>
+    return <DndProvider backend={HTML5Backend}>
         <div className="w-screen flex justify-center py-3 gap-x-2">
             <input className="w-1/2 h-8 rounded-lg text-black" value={item} onChange={handleChange} placeholder="Please enter your item!"></input>
             <button className="w-fit px-2 rounded-lg bg-white text-black" onClick={handleAddItem}>Add</button>
         </div>
-    </>
+        <div className="w-screen flex">
+            {
+                list.map((item,index) => <div id={item.id.toString()} key={item.id} draggable='true' onDragEnd={handleDragEnd} className="max-w-96 max-h-32 w-fit h-fit fixed p-2 rounded-lg bg-[#fede67] text-black" style={{top: item.y, left: item.x}} >{item.message}</div>)
+            }
+        </div>
+    </DndProvider>
 }
