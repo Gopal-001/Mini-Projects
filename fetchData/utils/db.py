cbc53ce1a7a8146f79d39811d2d3ec09e0f1be5c
_sameer <sameer.mishra@infra.market>
2024-09-15 22:13:11 +0530

    added db_url

--- fetchData/utils/db.py
+++ fetchData/utils/db.py
@@ -7,16 +7,24 @@ parser = ConfigParser()
 parser.read('config.ini')
 env = os.getenv('ENV', 'DEV')
 
-DB_NAME = os.getenv('DB_NAME')
-DB_USERNAME = os.getenv('DB_USERNAME')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_URL = os.getenv('DB_URL')
+DB_NAME = os.getenv('DB_NAME', parser.get(env, 'DB_NAME'))
+DB_USERNAME = os.getenv('DB_USERNAME', parser.get(env, 'DB_USERNAME'))
+DB_PASSWORD = os.getenv('DB_PASSWORD', parser.get(env, 'DB_PASSWORD'))
+DB_HOST = os.getenv('DB_HOST', parser.get(env, 'DB_HOST'))
+DB_PORT = os.getenv('DB_PORT', parser.get(env, 'DB_PORT'))
+DB_URL = os.getenv('DB_URL', parser.get(env, 'DB_URL'))
 
 def fetch_one_result(query, data=None):
     try:
-        conn = psycopg2.connect(DB_URL)
+        # conn = psycopg2.connect(DB_URL)
+        conn = psycopg2.connect(
+            user=DB_USERNAME,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT,
+            dbname=DB_NAME,
+            sslmode='require'
+        )   
         conn.set_session(autocommit = True)
         cursor = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
         cursor.execute(query, data)
