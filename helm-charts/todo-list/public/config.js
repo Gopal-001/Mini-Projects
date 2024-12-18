31678d0123da2e374a489fdcdc073bcb4e1cea1b
_sameer <sameer.mishra@infra.market>
2024-12-18 16:33:02 +0530

    feat: added a project to dynamically overwrite values from helm charts in docker

---
+++ helm-charts/todo-list/public/config.js
@@ -0,0 +1,4 @@
+window.RUNTIME_CONFIG={
+    URL_V1:"http://localhost:8080",
+    URL_V2:"http://localhost:8081"
+}
---
+++ helm-charts/todo-list/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Current url: {window.RUNTIME_CONFIG?.URL_V1}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
